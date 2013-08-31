4f11fa6ed4cc81e017b8cc46c34f8d3efe508562
JulianMayorga <mayorga.julian@gmail.com>
2013-08-31 05:19:00 -0300

    Ordenar puntajes de mayor a menor

--- js/puntajes.js
+++ js/puntajes.js
@@ -1,12 +1,21 @@
 var listRef;
-//var jugadores = [];
+var jugadores = [];
 
 $(function () {
     listRef = new Firebase('https://plasticidio.firebaseio.com/');
     listRef.on('child_added', function (snapshot) {
         var msgData = snapshot.val();
-        //jugadores.push(msgData.user_id + ": " + msgData.text + "seg");
-        $("#jugadores").append("<li><div class='jugador large button expand'>" + msgData.user_id + ": " + msgData.text + " seg</div></li>");
-        //console.log(jugadores);
+        jugadores.push({
+            "nombre": msgData.user_id,
+            "puntaje": msgData.text
+        });
+        jugadores.sort(numOrdDesc);
+        $("#jugadores").empty();
+        jugadores.forEach(function (element, index, array) {
+            $("#jugadores").append("<li><div class='jugador large button expand'>" + element.nombre + ": " + element.puntaje + " seg</div></li>");
+        })
+        console.log(jugadores);
     });
 });
+
+function numOrdDesc(a, b) { return (b.puntaje - a.puntaje); }
