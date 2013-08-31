ab0054c5b9ee2d9f1d39a0f11b078c0e6daa139e
JulianMayorga <mayorga.julian@gmail.com>
2013-08-31 00:02:16 -0300

    Agregar volcan y tierra en gamequery

---
+++ js/apocalipsis.js
@@ -0,0 +1,41 @@
+
+$(function () {
+    var tierra = new $.gQ.Animation({ imageURL: "img/tierra.png" });
+    var volcan = new $.gQ.Animation({ imageURL: "img/volcan.png" });
+    // sets the div to use to display the game and its dimension
+    $("#playground").playground({ width: 480, height: 800 });
+
+    // configure the loading bar
+    $.loadCallback(function (percent) {
+        $("#loadBar").width(400 * percent);
+        $("#loadtext").html("" + percent + "%");
+    });
+
+    // register the start button and remove the splash screen once the game is ready to starts
+    $("#start").click(function () {
+        $.playground().startGame(function () {
+            $("#splash").remove();
+
+            $.playground().addSprite("tierra", {
+                posx: 40,
+                posy: 200,
+                height: 400,
+                width: 400,
+                animation: tierra,
+                geometry: $.gQ.GEOMETRY_DISC /*GEOMETRY_DISC GEOMETRY_RECTANGLE*/
+            })
+
+            $.playground().addSprite("volcan", {
+                posx: 60,
+                posy: 200,
+                height: 101,
+                width: 58,
+                animation: volcan,
+                geometry: $.gQ.GEOMETRY_RECTANGLE /*GEOMETRY_DISC GEOMETRY_RECTANGLE*/
+            })
+
+            $("#volcan").rotate(-15);
+        });
+    });
+
+});
